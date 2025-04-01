f975c9d09c9e0d9314fbb630ddcdd61bce71c7f1
tj <thinupavani@gmail.com>
2025-04-01 08:18:28 +0530

    footer fixed

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,9 +7,9 @@ import Button from "@/components/ReusableComponents/Button";
 
 const Footer = () => {
   return (
-    <div className="footer position-relative">
+    <div className="footer position-relative mt-5" style={{backgroundColor: "#F2EDE6"}}>
 
-      <svg width="1900" height="738" viewBox="0 0 1200 500" fill="none" xmlns="http://www.w3.org/2000/svg" className='footer-backg'>
+      {/* <svg width="100%" height="100%" viewBox="0 0 1200 500" fill="none" xmlns="http://www.w3.org/2000/svg" className='footer-backg'>
         <g clipPath="url(#clip0_711_4869)">
           <rect width="1440" height="738" fill="#F3EEE7" />
           <path d="M376.779 30.0272C219.86 36.3225 0 89.2316 0 89.2316V-83H1440V89.2316C1440 89.2316 1382.81 108.974 1302.06 126.14C1246.98 137.852 1180.93 148.364 1114.6 150.59C848.186 159.529 643.086 19.3437 376.779 30.0272Z" fill="white" />
@@ -19,8 +19,12 @@ const Footer = () => {
             <rect width="1440" height="738" fill="white" />
           </clipPath>
         </defs>
-      </svg>
-      <div className='col-12 position-absolute footer-content'>
+      </svg> */}
+      <svg width="100%" height="100%" viewBox="0 0 1440 151" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M376.779 30.0272C219.86 36.3225 0 89.2316 0 89.2316V-83H1440V89.2316C1440 89.2316 1382.81 108.974 1302.06 126.14C1246.98 137.852 1180.93 148.364 1114.6 150.59C848.186 159.529 643.086 19.3437 376.779 30.0272Z" fill="white"/>
+</svg>
+
+      <div className='col-12 position-relative footer-content'>
         <div className='row footer-row'>
           <div className='col-12 col-md-8 col-lg-8 d-flex justify-content-start mb-5 mb-md-0 mb-lg-0'>
             <a className="navbar-brand" href="#">
