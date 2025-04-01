0060af9f3ece534a3f12cd5b3b82664d527c736d
tj <thinupavani@gmail.com>
2025-04-01 16:24:49 +0530

    update

--- components/servicesComponents/DescriptionSection.tsx
+++ components/servicesComponents/DescriptionSection.tsx
@@ -10,8 +10,8 @@ interface MainTitleProps {
 
 const DescriptionSection: React.FC<MainTitleProps> = (props) => {
     return (
-        <div className={`d-flex position-relative flex-column justify-content-center align-items-center mb-5`} style={{ width: "100%", overflow: "hidden" }}>
-            {/* <svg
+        <div className={`d-flex position-relative flex-column justify-content-center align-items-center ${style.mobileHeight} mb-5`} style={{ width: "100%", overflow: "hidden" }}>
+            <svg
                 className={style.shapedBg}
                 width="100%"
                 height="70%"
@@ -21,7 +21,7 @@ const DescriptionSection: React.FC<MainTitleProps> = (props) => {
             >
                 <path d="M0 271.5V0C0 0 454.705 90.4554 740.592 90.5C1026.84 90.5446 1448 0 1448 0V271.5H0Z" fill="#F3EEE7" />
                 <path d="M380.779 384.027C223.86 390.322 4 443.232 4 443.232V271H1444V443.232C1444 443.232 1386.81 462.974 1306.06 480.14C1250.98 491.852 1184.93 502.364 1118.6 504.59C852.186 513.529 647.086 373.344 380.779 384.027Z" fill="#F3EEE7" />
-            </svg> */}
+            </svg>
 
             <div
                 className={`${style.sectionShaped} ${pageStyle.contactContainer} d-flex flex-column justify-content-center align-items-center w-100 text-center fade-in-up`}
