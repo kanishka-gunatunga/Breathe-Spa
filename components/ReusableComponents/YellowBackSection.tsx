4b7e3c0e0e41da4403b90e6016bddc333519b6c7
tj <thinupavani@gmail.com>
2025-04-25 13:30:54 +0530

    update

--- components/ReusableComponents/YellowBackSection.tsx
+++ components/ReusableComponents/YellowBackSection.tsx
@@ -2,9 +2,11 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from "@/styles/page.module.css";
-import Button from "@/components/ReusableComponents/Button";
 import MainTitle from '../servicesComponents/MainTitle';
 import Paragraph from '../servicesComponents/Paragraph';
+import Link from 'next/link';
+import pageStyle from '@/styles/services.module.css'
+
 
 interface YellowBackSectionProps {
     title: string;
@@ -15,24 +17,26 @@ interface YellowBackSectionProps {
 }
 
 const YellowBackSection: React.FC<YellowBackSectionProps> = ({ title, description, image, buttonText, buttonLink }) => {
-    
+
     return (
 
         <div className={`d-flex position-relative flex-column justify-content-center align-items-center`} style={{ width: "100%", overflow: "hidden" }}>
             <div className={`${styles.shapedBg} py-5`}>
                 <div className={`${styles.contactContainer} section py-0`}>
-                    <div className='w-100'>
-                        <div className="d-flex d-lg-flex d-xl-flex flex-column-reverse flex-lg-row gap-5 w-100 py-0 py-lg-5 mb-5">
+                    <div className='w-100 py-5'>
+                        <div className="d-flex d-lg-flex d-xl-flex flex-column-reverse flex-lg-row w-100 py-0 py-lg-5 mb-5">
 
-                            <div className={`col-12 col-lg-6 d-flex flex-column ${styles.locationContainer}`}>
+                            <div className={`col-12 col-lg-6 pe-lg-4 d-flex flex-column justify-content-between ${styles.locationContainer}`}>
                                 <div className="margin_bottom_150">
                                     <MainTitle title={title} />
                                     <Paragraph text={description} />
                                 </div>
-                                <Button text={buttonText} href={buttonLink} ></Button>
+                                <Link href={buttonLink}><button type="button" className={`${pageStyle.darkButton}`} style={{fontSize: "18px !important", fontWeight: 600}}>
+                                    {buttonText}
+                                </button></Link>
                             </div>
 
-                            <div className="col-12 col-lg-6 d-flex justify-content-center ">
+                            <div className="col-12 col-lg-6 ps-lg-4 d-flex justify-content-center mb-3 mb-lg-0">
                                 <Image src={image} alt="Location" width={640} height={731} className="img-fluid rounded" />
                             </div>
 
