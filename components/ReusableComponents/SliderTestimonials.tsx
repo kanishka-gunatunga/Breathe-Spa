c80e376701437430dc25606e02c29893d9c94ebd
kSulakshana <58368165+ksulakshana02@users.noreply.github.com>
2025-06-11 12:30:37 +0530

    Update SliderTestimonials.tsx

--- components/ReusableComponents/SliderTestimonials.tsx
+++ components/ReusableComponents/SliderTestimonials.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Navigation, Pagination} from 'swiper/modules';
-// import Image from 'next/image';
 
 
 import 'swiper/css';
